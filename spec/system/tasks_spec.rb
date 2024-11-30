ee2bd4c7685323ec6343baba15c1b6ce84e5af35
Ulrichboris33 <ulrichgohoue@gmail.com>
2024-11-30 21:32:58 +0100

    validation de step

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,64 +1,31 @@
 require 'rails_helper'
-require 'factory_bot'
 
 RSpec.describe 'Fonction de gestion des tâches', type: :system do
-  describe "Fonction d'enregistrement" do
-    context "Lors de l'enregistrement d'une tâche" do
-      it "La tâche enregistrée s'affiche" do
-            # Enregistrer une tâche à utiliser dans le test
-            task= Task.create!(title: 'Préparation', content: 'Créer une proposition.')
-            # Passer à l'écran de la liste des tâches
-            visit task_path(task)
-            # Attendez (confirmez / attendez) que la chaîne de caractères "création de document" soit incluse dans la page visitée (dans ce cas, l'écran de la liste des tâches).
-            expect(page).to have_content 'Préparation'
-            # Si le résultat de expect est "true", le résultat du test est affiché comme un succès, et si le résultat de expect est "false", le résultat du test est affiché comme un échec.
-        
-      end
-    end
-  end
-  
-  describe "Fonction d'affichage de liste" do
-    context "Lors de la transition vers l'écran de liste" do
-      it "Une liste des tâches enregistrées s'affiche" do
-          # Enregistrer une tâche à utiliser dans le test
-          Task.create!(title: 'Préparation des documents', content: 'Créer une proposition.')
-          # Passer à l'écran de la liste des tâches
-          visit tasks_path
-          # Attendez (confirmez / attendez) que la chaîne de caractères "création de document" soit incluse dans la page visitée (dans ce cas, l'écran de la liste des tâches).
-          expect(page).to have_content 'Préparation des documents'
-          # Si le résultat de expect est "true", le résultat du test est affiché comme un succès, et si le résultat de expect est "false", le résultat du test est affiché comme un échec.
-      
-      end
-    end
-  end
-   
-  describe "Fonction d'affichage de liste" do
-    context "Lors de la transition vers l'écran de liste" do
-      it "Une liste des tâches enregistrées s'affiche" do
-          # Enregistrer une tâche à utiliser dans le test
-          FactoryBot.create(:task)
-          # Passer à l'écran de la liste des tâches
-          visit tasks_path
-          # Attendez (confirmez / attendez) que la chaîne de caractères "création de document" soit incluse dans la page visitée (dans ce cas, l'écran de la liste des tâches).
-          expect(page).to have_content 'Préparation'
-          # Si le résultat de expect est "true", le résultat du test est affiché comme un succès, et si le résultat de expect est "false", le résultat du test est affiché comme un échec.
-      
-      end
-    end
-  end
+      describe 'Nouvelle fonction de création' do
+        context 'Lors de la création de nouvelle tâche' do
+          it 'La tâche créée saffiche' do
+            visit new_task_path
+            fill_in 'Titre', with: 'title_test'
+            fill_in 'Contenu', with: 'content_test'
+            click_on 'Créer une tâche'
+            expect(page).to have_content 'title_test'
 
-  describe "Fonction d'affichage détaillée" do
-     context "Lors de la transition vers un écran de détails de tâche" do
-       it "Le contenu de la tâche s'affiche" do
-          # Enregistrer une tâche à utiliser dans le test
-          task= Task.create!(title: 'Préparation', content: 'Créer une proposition.')
-          # Passer à l'écran de la liste des tâches
-          visit task_path(task)
-          # Attendez (confirmez / attendez) que la chaîne de caractères "création de document" soit incluse dans la page visitée (dans ce cas, l'écran de la liste des tâches).
-          expect(page).to have_content 'Créer une proposition.'
-          # Si le résultat de expect est "true", le résultat du test est affiché comme un succès, et si le résultat de expect est "false", le résultat du test est affiché comme un échec.
+          end
+        end
+      end
+
+      describe 'Fonction daffichage trié' do
+        context 'Si les tâches sont classées par ordre décroissant de date et de heure de création' do
+          it 'Les nouvelles tâches apparaissent en haut de la liste.' do
+            Task.create(title: 'task1', content: 'description1')
+            Task.create( title: 'task2', content: 'description2')
+            Task.create(title: 'task3', content: 'description3')
+            Task.create(title: 'task4', content: 'description4') 
+            visit tasks_path
+            tasks_list = all('td')
+            expect(tasks_list[0]).to have_content 'task4'
+          end
+        end
+      end
       
-       end
-     end
-  end
 end
